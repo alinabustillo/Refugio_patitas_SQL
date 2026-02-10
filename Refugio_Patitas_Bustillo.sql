7bdc7df9d89f16821d6a424643c2337b91efd59e
alinabustillo <alinabustillo@gmail.com>
2026-02-10 02:20:14 -0300

    Update Refugio_Patitas_Bustillo.sql

--- Refugio_Patitas_Bustillo.sql
+++ Refugio_Patitas_Bustillo.sql
@@ -1,173 +1,115 @@
--- MySQL Workbench Forward Engineering
+DROP DATABASE IF EXISTS refugio_patitas;
+CREATE DATABASE refugio_patitas;
+USE refugio_patitas;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+-- ---------------------------------------------------------
+-- TABLAS INDEPENDIENTES
+-- ---------------------------------------------------------
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema refugio_patitas
--- -----------------------------------------------------
+-- Tabla: RAZAS
+-- Define que animal es y su raza.
+CREATE TABLE RAZAS (
+    id_raza INT AUTO_INCREMENT,
+    nombre_raza VARCHAR(50) NOT NULL,
+    tipo_animal VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id_raza)
+);
 
--- -----------------------------------------------------
--- Schema refugio_patitas
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `refugio_patitas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `refugio_patitas` ;
+-- Tabla: VETERINARIOS
+-- Datos de los profesionales que atienden.
+CREATE TABLE VETERINARIOS (
+    id_veterinario INT AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    matricula VARCHAR(20) NOT NULL,
+    telefono VARCHAR(20),
+    PRIMARY KEY (id_veterinario)
+);
 
--- -----------------------------------------------------
--- Table `refugio_patitas`.`razas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`razas` (
-  `id_raza` INT NOT NULL AUTO_INCREMENT,
-  `nombre_raza` VARCHAR(50) NOT NULL,
-  `tipo_animal` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`id_raza`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+-- Tabla: LABORATORIOS
+-- Laboratorio que fabrica las vacunas y teléfono por urgencias/reclamos.
+CREATE TABLE LABORATORIOS (
+    id_laboratorio INT AUTO_INCREMENT,
+    nombre_laboratorio VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20),
+    PRIMARY KEY (id_laboratorio)
+);
 
+-- Tabla: ADOPTANTES
+-- Datos de las personas que quieren adoptar.
+CREATE TABLE ADOPTANTES (
+    id_adoptante INT AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    dni VARCHAR(15) NOT NULL,
+    edad INT NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    direccion VARCHAR(150) NOT NULL,
+    email VARCHAR(100),
+    PRIMARY KEY (id_adoptante)
+);
 
--- -----------------------------------------------------
--- Table `refugio_patitas`.`animales`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`animales` (
-  `id_animal` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NOT NULL,
-  `fecha_ingreso` DATE NOT NULL,
-  `fecha_nacimiento` DATE NULL DEFAULT NULL,
-  `peso` DECIMAL(5,2) NULL DEFAULT NULL,
-  `castrado` VARCHAR(2) NULL DEFAULT NULL,
-  `estado` VARCHAR(20) NOT NULL,
-  `id_raza` INT NOT NULL,
-  PRIMARY KEY (`id_animal`),
-  INDEX `id_raza` (`id_raza` ASC) VISIBLE,
-  CONSTRAINT `animales_ibfk_1`
-    FOREIGN KEY (`id_raza`)
-    REFERENCES `refugio_patitas`.`razas` (`id_raza`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+-- ---------------------------------------------------------
+-- TABLAS CON DEPENDENCIAS
+-- ---------------------------------------------------------
 
+-- Tabla: VACUNAS
+-- Lista de vacunas. Se conecta con Laboratorio.
+CREATE TABLE VACUNAS (
+    id_vacuna INT AUTO_INCREMENT,
+    nombre_vacuna VARCHAR(50) NOT NULL, -- Nombre comercial
+    principio_activo VARCHAR(100),
+    lote VARCHAR(50) NOT NULL,
+    id_laboratorio INT NOT NULL,
+    PRIMARY KEY (id_vacuna),
+    FOREIGN KEY (id_laboratorio) REFERENCES LABORATORIOS(id_laboratorio)
+);
 
--- -----------------------------------------------------
--- Table `refugio_patitas`.`adoptantes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`adoptantes` (
-  `id_adoptante` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NOT NULL,
-  `apellido` VARCHAR(50) NOT NULL,
-  `dni` VARCHAR(15) NOT NULL,
-  `edad` INT NOT NULL,
-  `telefono` VARCHAR(20) NOT NULL,
-  `direccion` VARCHAR(150) NOT NULL,
-  `email` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_adoptante`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+-- Tabla: ANIMALES
+-- Datos de las mascotas. Se conecta con RAZAS.
+CREATE TABLE ANIMALES (
+    id_animal INT AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    fecha_ingreso DATE NOT NULL,
+    fecha_nacimiento DATE,   
+    peso DECIMAL(5,2),
+    castrado VARCHAR(2),
+    estado VARCHAR(20) NOT NULL,  -- Ej: Disponible, Adoptado, En Tratamiento
+    id_raza INT NOT NULL,
+    PRIMARY KEY (id_animal),
+    FOREIGN KEY (id_raza) REFERENCES RAZAS(id_raza)
+);
 
+-- ---------------------------------------------------------
+-- TABLAS DE RELACIÓN
+-- ---------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `refugio_patitas`.`adopciones`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`adopciones` (
-  `id_adopcion` INT NOT NULL AUTO_INCREMENT,
-  `id_animal` INT NOT NULL,
-  `id_adoptante` INT NOT NULL,
-  `fecha_adopcion` DATE NOT NULL,
-  `detalle` VARCHAR(500) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_adopcion`),
-  INDEX `id_animal` (`id_animal` ASC) VISIBLE,
-  INDEX `id_adoptante` (`id_adoptante` ASC) VISIBLE,
-  CONSTRAINT `adopciones_ibfk_1`
-    FOREIGN KEY (`id_animal`)
-    REFERENCES `refugio_patitas`.`animales` (`id_animal`),
-  CONSTRAINT `adopciones_ibfk_2`
-    FOREIGN KEY (`id_adoptante`)
-    REFERENCES `refugio_patitas`.`adoptantes` (`id_adoptante`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+-- Tabla: ADOPCIONES
+-- Registra cuando un Animal se va con un Adoptante.
+CREATE TABLE ADOPCIONES (
+    id_adopcion INT AUTO_INCREMENT,
+    id_animal INT NOT NULL,
+    id_adoptante INT NOT NULL,
+    fecha_adopcion DATE NOT NULL,
+    detalle VARCHAR(500),  -- Comentarios relevantes de la adopcion.
+    PRIMARY KEY (id_adopcion),
+    FOREIGN KEY (id_animal) REFERENCES ANIMALES(id_animal),
+    FOREIGN KEY (id_adoptante) REFERENCES ADOPTANTES(id_adoptante)
+);
 
-
--- -----------------------------------------------------
--- Table `refugio_patitas`.`veterinarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`veterinarios` (
-  `id_veterinario` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NOT NULL,
-  `apellido` VARCHAR(50) NOT NULL,
-  `matricula` VARCHAR(20) NOT NULL,
-  `telefono` VARCHAR(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_veterinario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `refugio_patitas`.`laboratorios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`laboratorios` (
-  `id_laboratorio` INT NOT NULL AUTO_INCREMENT,
-  `nombre_laboratorio` VARCHAR(100) NOT NULL,
-  `telefono` VARCHAR(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_laboratorio`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+-- Tabla: HISTORIA_CLINICA
+-- Consultas generales y vacunas.
+CREATE TABLE HISTORIA_CLINICA (
+    id_historia INT AUTO_INCREMENT,
+    id_animal INT NOT NULL,
+    id_veterinario INT NOT NULL,
+    id_vacuna INT,
+    fecha_consulta DATE NOT NULL,
+    observaciones VARCHAR(500),   -- Espacio para diagnóstico
+    PRIMARY KEY (id_historia),
+    FOREIGN KEY (id_animal) REFERENCES ANIMALES(id_animal),
+    FOREIGN KEY (id_veterinario) REFERENCES VETERINARIOS(id_veterinario),
+    FOREIGN KEY (id_vacuna) REFERENCES VACUNAS(id_vacuna)
+);
 
 
--- -----------------------------------------------------
--- Table `refugio_patitas`.`vacunas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`vacunas` (
-  `id_vacuna` INT NOT NULL AUTO_INCREMENT,
-  `nombre_vacuna` VARCHAR(50) NOT NULL,
-  `principio_activo` VARCHAR(100) NULL DEFAULT NULL,
-  `lote` VARCHAR(50) NOT NULL,
-  `id_laboratorio` INT NOT NULL,
-  PRIMARY KEY (`id_vacuna`),
-  INDEX `id_laboratorio` (`id_laboratorio` ASC) VISIBLE,
-  CONSTRAINT `vacunas_ibfk_1`
-    FOREIGN KEY (`id_laboratorio`)
-    REFERENCES `refugio_patitas`.`laboratorios` (`id_laboratorio`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `refugio_patitas`.`historia_clinica`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `refugio_patitas`.`historia_clinica` (
-  `id_historia` INT NOT NULL AUTO_INCREMENT,
-  `id_animal` INT NOT NULL,
-  `id_veterinario` INT NOT NULL,
-  `id_vacuna` INT NULL DEFAULT NULL,
-  `fecha_consulta` DATE NOT NULL,
-  `observaciones` VARCHAR(500) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_historia`),
-  INDEX `id_animal` (`id_animal` ASC) VISIBLE,
-  INDEX `id_veterinario` (`id_veterinario` ASC) VISIBLE,
-  INDEX `id_vacuna` (`id_vacuna` ASC) VISIBLE,
-  CONSTRAINT `historia_clinica_ibfk_1`
-    FOREIGN KEY (`id_animal`)
-    REFERENCES `refugio_patitas`.`animales` (`id_animal`),
-  CONSTRAINT `historia_clinica_ibfk_2`
-    FOREIGN KEY (`id_veterinario`)
-    REFERENCES `refugio_patitas`.`veterinarios` (`id_veterinario`),
-  CONSTRAINT `historia_clinica_ibfk_3`
-    FOREIGN KEY (`id_vacuna`)
-    REFERENCES `refugio_patitas`.`vacunas` (`id_vacuna`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
