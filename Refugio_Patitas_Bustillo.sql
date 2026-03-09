b4cb23b1e5fabf831d49187ea342a5b53cf0c993
alinabustillo <alinabustillo@gmail.com>
2026-03-09 00:00:07 -0300

    Agrego columna sexo a la tabla ANIMALES

--- Refugio_Patitas_Bustillo.sql
+++ Refugio_Patitas_Bustillo.sql
@@ -72,6 +72,7 @@ CREATE TABLE ANIMALES (
     id_animal INT AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     fecha_ingreso DATE NOT NULL,
+    sexo VARCHAR(50) NOT NULL,
     fecha_nacimiento DATE,   
     peso DECIMAL(5,2),
     castrado VARCHAR(2),
@@ -112,6 +113,7 @@ CREATE TABLE HISTORIA_CLINICA (
     FOREIGN KEY (id_veterinario) REFERENCES VETERINARIOS(id_veterinario),
     FOREIGN KEY (id_vacuna) REFERENCES VACUNAS(id_vacuna)
 );
+
 
 
 
